ae2f9afb3cf2c1dc101dc9d5a1579ab8b8da5c6e
João Silva <jgc3silva@gmail.com>
2019-03-19 15:18:18 +0000

    USART work

--- software/src/usart.c
+++ software/src/usart.c
@@ -16,7 +16,7 @@ void usart0_init(uint32_t ulBaud, uint8_t ubMode, uint8_t ubBitMode, int8_t bMIS
 
     USART0->CMD = USART_CMD_CLEARRX | USART_CMD_CLEARTX | UART_CMD_TXTRIDIS | USART_CMD_RXBLOCKDIS | USART_CMD_TXDIS | USART_CMD_RXDIS;
 
-    USART0->CTRL = USART_CTRL_TXBIL_EMPTY | USART_CTRL_CSMA_NOACTION | (ubBitMode == USART_SPI_MSB_FIRST ? USART_CTRL_MSBF : 0) | (ubBitMode & 1 ? USART_CTRL_CLKPHA_SAMPLETRAILING : USART_CTRL_CLKPHA_SAMPLELEADING) | (ubBitMode & 2 ? USART_CTRL_CLKPOL_IDLEHIGH : USART_CTRL_CLKPOL_IDLELOW) | USART_CTRL_SYNC;
+    USART0->CTRL = USART_CTRL_TXBIL_EMPTY | USART_CTRL_CSMA_NOACTION | (ubBitMode == USART_SPI_MSB_FIRST ? USART_CTRL_MSBF : 0) | (ubMode & 1 ? USART_CTRL_CLKPHA_SAMPLETRAILING : USART_CTRL_CLKPHA_SAMPLELEADING) | (ubMode & 2 ? USART_CTRL_CLKPOL_IDLEHIGH : USART_CTRL_CLKPOL_IDLELOW) | USART_CTRL_SYNC;
     USART0->FRAME = USART_FRAME_DATABITS_EIGHT;
     USART0->CLKDIV = ((HFPER_CLOCK_FREQ / (2 * ulBaud)) - 1) << 8;
 
@@ -60,22 +60,88 @@ static volatile uint16_t usUSART0FIFOWritePos, usUART2FIFOReadPos;
 
 void usart0_init(uint32_t ulBaud, uint32_t ulFrameSettings, int8_t bRXLocation, int8_t bTXLocation, int8_t bCTSLocation, int8_t bRTSLocation)
 {
+    if(bRXLocation > AFCHANLOC_MAX)
+        return;
 
+    if(bTXLocation > AFCHANLOC_MAX)
+        return;
+
+    if(bCTSLocation > AFCHANLOC_MAX)
+        return;
+
+    if(bRTSLocation > AFCHANLOC_MAX)
+        return;
+
+    CMU->HFPERCLKEN0 |= CMU_HFPERCLKEN0_USART0;
+
+    USART0->CMD = USART_CMD_CLEARRX | USART_CMD_CLEARTX | UART_CMD_TXTRIDIS | USART_CMD_RXBLOCKDIS | USART_CMD_TXDIS | USART_CMD_RXDIS;
+
+    free((uint8_t *)pubUSART0DMABuffer);
+    free((uint8_t *)pubUSART0FIFO);
+
+    pubUSART0DMABuffer = (volatile uint8_t *)malloc(USART0_DMA_RX_BUFFER_SIZE);
+
+    if(!pubUSART0DMABuffer)
+        return;
+
+    memset((uint8_t *)pubUSART0DMABuffer, 0, USART0_DMA_RX_BUFFER_SIZE);
+
+    pubUSART0FIFO = (volatile uint8_t *)malloc(USART0_FIFO_SIZE);
+
+    if(!pubUSART0FIFO)
+    {
+        free((void *)pubUSART0DMABuffer);
+
+        return;
+    }
+
+    memset((uint8_t *)pubUSART0FIFO, 0, USART0_FIFO_SIZE);
+
+    usUSART0FIFOWritePos = 0;
+    usUART2FIFOReadPos = 0;
+
+    USART0->CTRL = USART_CTRL_TXBIL_EMPTY | USART_CTRL_CSMA_NOACTION | UART_CTRL_OVS_X16;
+    USART0->CTRLX = (bCTSLocation >= 0 ? USART_CTRLX_CTSEN : 0);
+    USART0->FRAME = ulFrameSettings;
+    USART0->CLKDIV = ((HFPER_CLOCK_FREQ / (16 * ulBaud)) - 1) << 8;
+
+    USART0->TIMECMP0 = UART_TIMECMP0_TSTOP_RXACT | UART_TIMECMP0_TSTART_RXEOF | 0x08; // RX Timeout after 8 baud times
+
+    USART0->ROUTEPEN = (bRXLocation >= 0 ? USART_ROUTEPEN_RXPEN : 0) | (bTXLocation >= 0 ? USART_ROUTEPEN_TXPEN : 0) | (bCTSLocation >= 0 ? USART_ROUTEPEN_CTSPEN : 0) | (bRTSLocation >= 0 ? USART_ROUTEPEN_RTSPEN : 0);
+    USART0->ROUTELOC0 = ((uint32_t)(bRXLocation >= 0 ? bRXLocation : 0) << _USART_ROUTELOC0_RXLOC_SHIFT) | ((uint32_t)(bTXLocation >= 0 ? bTXLocation : 0) << _USART_ROUTELOC0_TXLOC_SHIFT);
+    USART0->ROUTELOC1 = ((uint32_t)(bCTSLocation >= 0 ? bCTSLocation : 0) << _USART_ROUTELOC1_CTSLOC_SHIFT) | ((uint32_t)(bRTSLocation >= 0 ? bRTSLocation : 0) << _USART_ROUTELOC1_RTSLOC_SHIFT);
+
+    USART0->CMD = (bTXLocation >= 0 ? USART_CMD_TXEN : 0) | (bRXLocation >= 0 ? USART_CMD_RXEN : 0);
 }
 void usart0_write_byte(const uint8_t ubData)
 {
+    while(!(USART0->STATUS & USART_STATUS_TXBL));
 
+    USART0->TXDATA = ubData;
 }
 uint8_t usart0_read_byte()
 {
+    if(!usart0_available())
+        return 0;
 
+    uint8_t ubData;
+
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
+    {
+        ubData = pubUSART0FIFO[usUART2FIFOReadPos++];
+
+        if(usUART2FIFOReadPos >= USART0_FIFO_SIZE)
+            usUART2FIFOReadPos = 0;
+    }
+
+    return ubData;
 }
 uint32_t usart0_available()
 {
-
+    return (USART0_FIFO_SIZE + usUSART0FIFOWritePos - usUART2FIFOReadPos) % USART0_FIFO_SIZE;
 }
 void usart0_flush()
 {
-
+    usUART2FIFOReadPos = usUSART0FIFOWritePos = 0;
 }
 #endif  // USART0_MODE
