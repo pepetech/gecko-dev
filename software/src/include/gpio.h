5068d27c15023745c96403bc4165dbdcc6e7099a
João Silva <jgc3silva@gmail.com>
2019-03-19 14:37:46 +0000

    LDMA work

--- software/src/include/gpio.h
+++ software/src/include/gpio.h
@@ -4,8 +4,8 @@
 #include <em_device.h>
 #include "utils.h"
 
-#define LED_ON()        PERI_REG_BIT_SET(&GPIO->P[0].DOUT) = BIT(0)
-#define LED_OFF()       PERI_REG_BIT_CLEAR(&GPIO->P[0].DOUT) = BIT(0)
+#define LED_ON()        PERI_REG_BIT_SET(&(GPIO->P[0].DOUT)) = BIT(0)
+#define LED_OFF()       PERI_REG_BIT_CLEAR(&(GPIO->P[0].DOUT)) = BIT(0)
 #define LED_TOGGLE()    GPIO->P[0].DOUTTGL = BIT(0);
 
 void gpio_init();
--- software/src/ldma.c
+++ software/src/ldma.c
@@ -1,9 +1,38 @@
 #include "ldma.h"
 
+static ldma_ch_isr_t ppfChannelISR[DMA_CHAN_COUNT];
+
+void _ldma_isr()
+{
+    uint32_t ulFlags = LDMA->IFC;
+
+    if(ulFlags & LDMA_IFC_ERROR)
+    {
+        uint8_t ubErrorChannel = (LDMA->STATUS & _LDMA_STATUS_CHERROR_MASK) << _LDMA_STATUS_CHERROR_SHIFT;
+
+        if(ppfChannelISR[ubErrorChannel])
+            ppfChannelISR[ubErrorChannel](1);
+    }
+
+    for(uint8_t ubChannel = 0; ubChannel < DMA_CHAN_COUNT; ubChannel++)
+    {
+        if(!(ulFlags & BIT(ubChannel)))
+            continue;
+
+        if(ppfChannelISR[ubChannel])
+            ppfChannelISR[ubChannel](0);
+    }
+}
 
 void ldma_init()
 {
     CMU->HFBUSCLKEN0 |= CMU_HFBUSCLKEN0_LDMA;
+
+    LDMA->IFC = _LDMA_IFC_MASK; // Clear all flags
+    IRQ_CLEAR(LDMA_IRQn); // Clear pending vector
+    IRQ_SET_PRIO(LDMA_IRQn, 2, 0); // Set priority 2,0
+    IRQ_ENABLE(LDMA_IRQn); // Enable vector
+    LDMA->IEN = _LDMA_IEN_MASK; // Enable all channel flags
 }
 
 void ldma_sync_set(uint8_t ubMask)
@@ -21,7 +50,15 @@ void ldma_ch_config(uint8_t ubChannel, uint32_t ulSource, uint32_t ulSrcIncSign,
         return;
 
     LDMA->CH[ubChannel].REQSEL = ulSource;
-    LDMA->CH[ubChannel].CFG = ulSrcIncSign ;
+    LDMA->CH[ubChannel].CFG = ulDstIncSign | ulSrcIncSign | ulArbitrationSlots;
+    LDMA->CH[ubChannel].LOOP = ubLoopCount;
+}
+void ldma_ch_set_isr(uint8_t ubChannel, ldma_ch_isr_t pfISR)
+{
+    if(ubChannel >= DMA_CHAN_COUNT)
+        return;
+
+    ppfChannelISR[ubChannel] = pfISR;
 }
 void ldma_ch_load(uint8_t ubChannel, ldma_descriptor_t *pDescriptor)
 {
@@ -36,6 +73,7 @@ void ldma_ch_load(uint8_t ubChannel, ldma_descriptor_t *pDescriptor)
 
     LDMA->CH[ubChannel].LINK = (uint32_t)pDescriptor;
 
+    PERI_REG_BIT_CLEAR(&(LDMA->CHDONE)) = BIT(ubChannel);
     LDMA->LINKLOAD = BIT(ubChannel);
 }
 void ldma_ch_req(uint8_t ubChannel)
@@ -58,4 +96,16 @@ void ldma_ch_disable(uint8_t ubChannel)
         return;
 
     PERI_REG_BIT_CLEAR(&(LDMA->CHEN)) = BIT(ubChannel);
+}
+uint16_t ldma_ch_get_remaining_xfers(uint8_t ubChannel)
+{
+    if(ubChannel >= DMA_CHAN_COUNT)
+        return 0;
+
+    uint16_t usRemaining = (LDMA->CH[ubChannel].CTRL & _LDMA_CH_CTRL_XFERCNT_MASK) >> _LDMA_CH_CTRL_XFERCNT_SHIFT;
+
+    if((LDMA->CHDONE & BIT(ubChannel)) || (!usRemaining && (LDMA->IF & BIT(ubChannel))))
+        return 0;
+
+    return usRemaining + 1;
 }
